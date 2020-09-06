1085e5a6a372dfb049fbb9448b24df55288f43f1
achlanbima <achlan_00011101@hotmail.com>
2020-09-06 21:18:55 +0700

    use table & use placeholder image

--- src/components/product-list/ProductList.js
+++ src/components/product-list/ProductList.js
@@ -4,17 +4,41 @@ import { actionGetProducts } from '../../_redux/actions/Products';
 
 const Index = (props) => {
 
+    const { fetchProducts } = props;
+
     useEffect(()=>{
-        props.fetchProducts();
-    },[]);
+        fetchProducts();
+    },[fetchProducts]);
 
     return (
         <div>
-            {props.products.data.map(product=>(
+            {/* {props.products.data.map(product=>(
                 <div key={product.sku}>
                     <p>{product.name} <span style={{fontWeight:500}}>${product.unitPrice}</span></p>
                 </div>
-            ))}
+            ))} */}
+            <table className="table">
+                <thead className="thead-dark">
+                    <tr>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Unit in Stock</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.products.data.map(product=>(
+                        <tr key={product.sku}>
+                            <td className="align-middle">
+                                <img src={process.env.PUBLIC_URL + `/${product.imageUrl}`} alt={product.image_url} height={50} />
+                            </td>
+                            <td className="align-middle">{product.name}</td>
+                            <td className="align-middle">$ {product.unitPrice}</td>
+                            <td className="align-middle">{product.unitsInStock}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
