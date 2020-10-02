501ace13ac7c613e1f1fe23fc78f140ce851000c
muhammad.nur <muhammad.nur@metraplasa.co.id>
2020-10-02 11:19:38 +0700

    Adding handle error in redux and fix style css

--- src/_redux/actions/Products.js
+++ src/_redux/actions/Products.js
@@ -1,12 +1,16 @@
-import ecommerceApi from '../../api/ecommerce'
+import ecommerceApi from '../../api/ecommerce';
 
 export const actionGetProducts = () => {
-    return {
-        type: 'GET_PRODUCTS',
-        payload: async () => {
-            const res = await ecommerceApi.get('/products');
-            console.log('test',res);
-            return res.data._embedded.products
-        }
-    }
-}
+  return {
+    type: 'GET_PRODUCTS',
+    payload: async () => {
+      try {
+        const res = await ecommerceApi.get('/products');
+        console.log('test', res);
+        return res.data._embedded.products;
+      } catch (error) {
+        return error.message;
+      }
+    },
+  };
+};
--- src/_redux/reducers/Products.js
+++ src/_redux/reducers/Products.js
@@ -1,32 +1,35 @@
 const initialState = {
-    data: [],
-    loading: false,
-    error: false
-}
+  data: [],
+  loading: false,
+  error: false,
+  message: '',
+};
 
 const Products = (state = initialState, action) => {
-    switch(action.type){
-        case 'GET_PRODUCTS_PENDING':
-            return {
-                ...state,
-                loading: true
-            }
-        
-        case 'GET_PRODUCTS_FULFILLED':
-            return {
-                ...state,
-                loading: false,
-                data: action.payload
-            }
-        case 'GET_PRODUCTS_REJECTED':
-            return {
-                ...state,
-                loading: false,
-                error: true
-            }
-        default: 
-            return state;
-    }
-}
+  switch (action.type) {
+    case 'GET_PRODUCTS_PENDING':
+      return {
+        ...state,
+        loading: true,
+      };
 
-export default Products
+    case 'GET_PRODUCTS_FULFILLED':
+      return {
+        ...state,
+        loading: false,
+        message: '',
+        data: action.payload,
+      };
+    case 'GET_PRODUCTS_REJECTED':
+      return {
+        ...state,
+        loading: false,
+        error: true,
+        message: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default Products;
